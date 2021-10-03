4ed0d15176f9e8e072fd77778b5ae1acdf0d3500
Michael Sanchez <michael.joshua.sanchez@gmail.com>
2021-10-03 16:52:38 -0700

    10/3/21 data types and list comprehension exercise

---
+++ data_types_and_variables.py
@@ -0,0 +1,37 @@
+def movie_rental():
+    little_mermaid = 3
+    brother_bear = 5
+    hercules = 1
+    total_days = little_mermaid + brother_bear + hercules
+    price_per_day = 3
+
+    total_price = price_per_day * total_days
+
+
+def weekly_pay():
+    google_pay = 400
+    amazon_pay = 380
+    facebook_pay = 350
+
+    google_hrs = 6
+    amazon_hrs = 4
+    facebook_hrs = 10
+
+    pay =  () google_pay * googe_hrs ) + (amazon_pay * amazon_hrs) + (facebook_pay * facebook_hrs)
+
+def can_enroll(class_, class_period, class_size, max_size):
+    
+    if class_period not in student.classes and class_size < max_size:
+
+        return True
+
+def do_prod_offer(total_items, exp_date, is_prem):
+    if exp_date > datetime.date():
+        if total_items > 2 or is_prem:
+            return True
+    return False
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+bool_ = len(password) >= 5 and len(username) <= 20 and username != password
